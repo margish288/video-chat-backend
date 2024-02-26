119fa134470d78888c4d21ec77a148762e8885dd
margish <margish@socialpilot.co>
2024-02-27 02:15:38 +0530

    Add : user Join to room

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import { Server } from "socket.io";
 
 const PORT = 8000;
-const io = new Server(PORT);
+const io = new Server(PORT, {
+  cors: true,
+});
+
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
 
 io.on("connection", (socket) => {
   console.log("Socket connected - ", socket.id);
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    console.log(data);
+    emailToSocketIdMap.set(email, socket.id);
+    socketIdToEmailMap.set(socket.id, email);
+
+    io.to(room).emit("room:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
 });
